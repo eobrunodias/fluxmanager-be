6ce59a424c427ebb6a30f5d3b3f79d22b46b0ca0
Bruno Dias <brunodias.contact@gmail.com>
2025-12-07 22:35:36 +0000

    feat(product): add CreateProductDto with validation rules

--- src/modules/products/dto/create-product.dto.ts
+++ src/modules/products/dto/create-product.dto.ts
@@ -1,1 +1,41 @@
-export class CreateProductDto {}
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+  Length,
+  MaxLength,
+  Min,
+} from "class-validator";
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 100)
+  name: string;
+
+  @IsNotEmpty()
+  @Length(5, 50)
+  @IsString()
+  sku: string;
+
+  @IsNotEmpty()
+  @Length(5, 30)
+  @IsString()
+  barcode: string;
+
+  @Min(0.01)
+  @IsNumber({ maxDecimalPlaces: 2 })
+  price: number;
+
+  @IsNotEmpty()
+  @MaxLength(255)
+  @IsString()
+  description: string;
+
+  @IsUUID()
+  categoryId: string;
+
+  @IsUUID()
+  supplierId: string;
+}
