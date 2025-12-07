3da81797c5f31f74c4ff15a3fa104a89ebd39d4a
Bruno Dias <brunodias.contact@gmail.com>
2025-12-07 22:40:21 +0000

    feat(category): add CreateCategoryDto with validation rules

--- src/modules/categories/dto/create-category.dto.ts
+++ src/modules/categories/dto/create-category.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsString, Length } from "class-validator";
+
+export class CreateCategoryDto {
+  @IsNotEmpty()
+  @Length(10, 100)
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 255)
+  description: string;
+}
