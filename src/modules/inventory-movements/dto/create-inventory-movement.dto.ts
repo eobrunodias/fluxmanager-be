a320a544bf79d75148f6f0020030e83b9f824ae6
Bruno Dias <brunodias.contact@gmail.com>
2025-12-07 22:39:22 +0000

    feat(inventory): add CreateInventoryMovementDto with validation and update entity

--- src/modules/inventory-movements/dto/create-inventory-movement.dto.ts
+++ src/modules/inventory-movements/dto/create-inventory-movement.dto.ts
@@ -1,1 +1,31 @@
-export class CreateInventoryMovementDto {}
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Min,
+} from "class-validator";
+import { InventoryMovementType } from "src/common/enums";
+
+export class CreateInventoryMovementDto {
+  @IsUUID()
+  productId: string;
+
+  @IsNotEmpty()
+  @IsEnum(InventoryMovementType)
+  type: InventoryMovementType;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  quantity: number;
+
+  @IsString()
+  @IsOptional()
+  note: string;
+
+  @IsUUID()
+  orderId: string;
+}
--- src/modules/inventory-movements/entities/inventory-movement.entity.ts
+++ src/modules/inventory-movements/entities/inventory-movement.entity.ts
@@ -1,26 +1,31 @@
 import { InventoryMovementType } from "src/common/enums";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 
 @Entity()
 export class InventoryMovement {
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
-  @Column({ unique: true, name: "product_id" })
+  @Column({ name: "product_id" })
   productId: string;
 
-  @Column({ type: "enum", enum: "InventoryMovementType" })
+  @Column({ type: "enum", enum: InventoryMovementType })
   type: InventoryMovementType;
 
-  @Column({ type: "int", precision: 5 })
+  @Column({ type: "int" })
   quantity: number;
 
-  @Column()
-  note: string;
+  @Column({ nullable: true })
+  note?: string;
 
   @Column({ name: "order_id" })
   orderId: string;
 
-  @Column({ name: "created_at" })
-  createdAt: string;
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
 }
