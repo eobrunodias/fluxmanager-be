7d90e62e4117cb02baade0ee9034078ca7a0b441
Bruno Dias <brunodias.contact@gmail.com>
2025-12-07 20:57:12 +0000

    refactor(tests): change the local unit tests

---
+++ src/modules/inventory-movements/test/inventory-movements.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { InventoryMovementsController } from "../inventory-movements.controller";
+import { InventoryMovementsService } from "../inventory-movements.service";
+
+describe("InventoryMovementsController", () => {
+  let controller: InventoryMovementsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [InventoryMovementsController],
+      providers: [InventoryMovementsService],
+    }).compile();
+
+    controller = module.get<InventoryMovementsController>(
+      InventoryMovementsController,
+    );
+  });
+
+  it("should be defined", () => {
+    expect(controller).toBeDefined();
+  });
+});
