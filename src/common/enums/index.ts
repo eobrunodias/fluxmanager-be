c7438162c461c668d23b47f0d5cca7ecd6127b2f
Bruno Dias <brunodias.contact@gmail.com>
2025-12-07 22:47:02 +0000

    fix(enums): remove export of order-item-unit-price.enum from index

--- src/common/enums/index.ts
+++ src/common/enums/index.ts
@@ -1,7 +1,6 @@
 export * from "./cashflow-type.enum";
 export * from "./currency-enum";
 export * from "./invetory-movement-type.enum";
-export * from "./order-item-unit-price.enum";
 export * from "./order-status.enum";
 export * from "./payment-method.enum";
 export * from "./payment-status.enum";
