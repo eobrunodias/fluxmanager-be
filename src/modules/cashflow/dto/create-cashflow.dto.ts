6983541b26b71057292576ae2e3dee5f5585c44e
Bruno Dias <brunodias.contact@gmail.com>
2025-12-07 22:40:11 +0000

    feat(cashflow): add CreateCashflowDto with validation rules

--- src/modules/cashflow/dto/create-cashflow.dto.ts
+++ src/modules/cashflow/dto/create-cashflow.dto.ts
@@ -1,1 +1,30 @@
-export class CreateCashflowDto {}
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUUID,
+  MaxLength,
+} from "class-validator";
+import { CashflowType } from "src/common/enums";
+
+export class CreateCashflowDto {
+  @IsNotEmpty()
+  @IsEnum(CashflowType)
+  type: CashflowType;
+
+  @IsUUID()
+  orderId: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(255)
+  description?: string;
+
+  @IsNotEmpty()
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  value: number;
+}
