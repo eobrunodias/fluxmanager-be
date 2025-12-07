6e528fa3e531035b4e64fd9e3f4e47b954a727cd
Bruno Dias <brunodias.contact@gmail.com>
2025-12-07 22:40:25 +0000

    feat(client): add CreateClientDto with validation rules

--- src/modules/clients/dto/create-client.dto.ts
+++ src/modules/clients/dto/create-client.dto.ts
@@ -1,1 +1,30 @@
-export class CreateClientDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  Length,
+} from "class-validator";
+
+export class CreateClientDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 100)
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @Length(100)
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsPhoneNumber()
+  @Length(10, 20)
+  phone: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 200)
+  address: string;
+}
