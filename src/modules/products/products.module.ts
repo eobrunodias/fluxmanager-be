c8ea0888c43d9c2db1498c45a22173039734dbc6
Bruno Dias <brunodias.contact@gmail.com>
2025-12-11 20:50:13 +0000

    feat: add ProductsService with validation and error handling for CRUD operations

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -3,9 +3,11 @@ import { ProductsService } from "./products.service";
 import { ProductsController } from "./products.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Product } from "./entities/product.entity";
+import { Category } from "../categories/entities/category.entity";
+import { Supplier } from "../suppliers/entities/supplier.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, Category, Supplier])],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,26 +1,113 @@
-import { Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { CreateProductDto } from "./dto/create-product.dto";
 import { UpdateProductDto } from "./dto/update-product.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Product } from "./entities/product.entity";
+import { Repository } from "typeorm";
+import { Category } from "../categories/entities/category.entity";
+import { Supplier } from "../suppliers/entities/supplier.entity";
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return "This action adds a new product";
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(Supplier)
+    private readonly supplierRepository: Repository<Supplier>,
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const existingBarcode = await this.productRepository.findOneBy({
+      barcode: createProductDto.barcode,
+    });
+    if (existingBarcode)
+      throw new BadRequestException("Barcode already exists");
+
+    const existingSku = await this.productRepository.findOneBy({
+      sku: createProductDto.sku,
+    });
+    if (existingSku) throw new BadRequestException("SKU already exists");
+
+    const category = await this.categoryRepository.findOne({
+      where: { id: createProductDto.categoryId },
+    });
+    if (!category) throw new NotFoundException("Category not found");
+
+    const supplier = await this.supplierRepository.findOne({
+      where: { id: createProductDto.supplierId },
+    });
+    if (!supplier) throw new NotFoundException("Supplier not found");
+
+    const product = {
+      ...createProductDto,
+      category,
+      supplier,
+    };
+
+    const productCreated = this.productRepository.create(product);
+    if (!productCreated) throw new ConflictException("Product not created");
+
+    const productSaved = await this.productRepository.save(productCreated);
+    if (!productSaved) throw new ConflictException("Product not saved");
+
+    return productSaved;
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    const products = await this.productRepository.find();
+    if (!products) throw new ConflictException("Products not found");
+
+    return products;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    if (!id) throw new BadRequestException("Id is required");
+
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) throw new ConflictException("Product not found");
+
+    return product;
   }
 
-  update(id: string, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    if (!id) throw new BadRequestException("Id is required");
+    if (!updateProductDto) new BadRequestException("Data product is required");
+
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) throw new ConflictException("Product not found");
+
+    const productUpdated = {
+      ...product,
+      ...updateProductDto,
+    };
+
+    const productPrealoaded =
+      await this.productRepository.preload(productUpdated);
+    if (!productPrealoaded)
+      throw new ConflictException("Product not preloaded");
+
+    const productSaved = await this.productRepository.save(productPrealoaded);
+    if (!productSaved) throw new ConflictException("Product not saved");
+
+    return product;
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    if (!id) throw new BadRequestException("Id is required");
+
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) throw new ConflictException("Product not found");
+
+    const productDeleted = await this.productRepository.delete({ id });
+    if (!productDeleted) throw new ConflictException("Product not deleted");
+
+    return product;
   }
 }
