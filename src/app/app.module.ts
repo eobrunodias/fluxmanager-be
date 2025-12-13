bdc3e2b4e47aa5cede959d4a243b5c0180b5cddc
Bruno Dias <brunodias.contact@gmail.com>
2025-12-13 17:42:49 +0000

    refactor: centralize module imports in app.module and index files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,15 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthModule } from "../modules/auth/auth.module";
-import { UsersModule } from "../modules/users/users.module";
-import { OrdersModule } from "../modules/orders/orders.module";
-import { OrderItemsModule } from "../modules/order-items/order-items.module";
-import { StockModule } from "../modules/stock/stock.module";
-import { PaymentsModule } from "../modules/payments/payments.module";
-import { ClientsModule } from "../modules/clients/clients.module";
-import { ProductsModule } from "../modules/products/products.module";
-import { SuppliersModule } from "../modules/suppliers/suppliers.module";
-import { CategoriesModule } from "../modules/categories/categories.module";
-import { SettingsModule } from "../modules/settings/settings.module";
-import { CashflowsModule } from "../modules/cashflows/cashflows.module";
-import { InvoicesModule } from "../modules/invoices/invoices.module";
-import { InventoryMovementsModule } from "../modules/inventory-movements/inventory-movements.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
+import { featureModules } from "src/modules";
 
 @Module({
   imports: [
-    AuthModule,
-    UsersModule,
-    OrdersModule,
-    OrderItemsModule,
-    StockModule,
-    PaymentsModule,
-    ClientsModule,
-    ProductsModule,
-    SuppliersModule,
-    CategoriesModule,
-    SettingsModule,
-    CashflowsModule,
-    InvoicesModule,
-    InventoryMovementsModule,
+    ...featureModules,
     ConfigModule.forRoot({
-      isGlobal: true, // deixa o .env disponível no app todo
+      isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
       useFactory: () => {
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,1 +1,31 @@
-// TODO: centralize every module here (as Type[]) like this
+import { CategoriesModule } from "./categories/categories.module";
+import { InvoicesModule } from "./invoices/invoices.module";
+import { OrdersModule } from "./orders/orders.module";
+import { ProductsModule } from "./products/products.module";
+import { SuppliersModule } from "./suppliers/suppliers.module";
+import { UsersModule } from "./users/users.module";
+import { InventoryMovementsModule } from "./inventory-movements/inventory-movements.module";
+import { StockModule } from "./stock/stock.module";
+import { PaymentsModule } from "./payments/payments.module";
+import { OrderItemsModule } from "./order-items/order-items.module";
+import { AuthModule } from "./auth/auth.module";
+import { CashflowsModule } from "./cashflows/cashflows.module";
+import { ClientsModule } from "./clients/clients.module";
+import { SettingsModule } from "./settings/settings.module";
+
+export const featureModules = [
+  UsersModule,
+  ProductsModule,
+  InvoicesModule,
+  OrdersModule,
+  SuppliersModule,
+  CategoriesModule,
+  InventoryMovementsModule,
+  StockModule,
+  PaymentsModule,
+  OrderItemsModule,
+  AuthModule,
+  CashflowsModule,
+  ClientsModule,
+  SettingsModule,
+];
