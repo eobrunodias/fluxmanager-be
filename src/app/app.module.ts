4db710f3a56c9f6363e9a9e75c018af5685d8d0b
Bruno Dias <brunodias.contact@gmail.com>
2025-12-15 16:07:14 +0000

    refactor: remove AppController and AppService; create repository files for various modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
 import { featureModules } from "src/modules";
@@ -26,7 +24,7 @@ import { featureModules } from "src/modules";
       },
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
