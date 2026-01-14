7f658fc0804e306a71db10e335ec9859dc1760fd
Bruno Dias <brunodias.contact@gmail.com>
2026-01-14 14:52:56 +0000

    feat: implement CategoriesRepository with CRUD operations and error handling

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,46 +1,26 @@
-import {
-  BadRequestException,
-  ConflictException,
-  Injectable,
-} from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { CreateCategoryDto } from "./dto/create-category.dto";
 import { UpdateCategoryDto } from "./dto/update-category.dto";
-import { Category } from "./entities/category.entity";
-import { Repository } from "typeorm";
-import { InjectRepository } from "@nestjs/typeorm";
+import { CategoriesRepository } from "./repositories/categories.repository";
 
 @Injectable()
 export class CategoriesService {
-  constructor(
-    @InjectRepository(Category)
-    private readonly repository: Repository<Category>,
-  ) {}
+  constructor(private readonly repository: CategoriesRepository) {}
 
   async create(createCategoryDto: CreateCategoryDto) {
-    const categoryCreated = this.repository.create(createCategoryDto);
-    if (!categoryCreated)
+    if (!createCategoryDto)
       throw new BadRequestException("Invalid data category");
 
-    const categorySaved = await this.repository.save(categoryCreated);
-    if (!categorySaved) throw new ConflictException("Category not saved");
-
-    return categorySaved;
+    return this.repository.createCategory(createCategoryDto);
   }
 
   async findAll() {
-    const categories = await this.repository.find();
-    if (!categories) throw new ConflictException("Categories not found");
-
-    return categories;
+    return this.repository.findAllCategories();
   }
 
   async findOne(id: string) {
     if (!id) throw new BadRequestException("Id is required");
-
-    const category = await this.repository.findOneBy({ id });
-    if (!category) throw new ConflictException("Category not found");
-
-    return category;
+    return await this.repository.findCategoryById(id);
   }
 
   async update(id: string, updateCategoryDto: UpdateCategoryDto) {
@@ -48,33 +28,11 @@ export class CategoriesService {
     if (!updateCategoryDto)
       throw new BadRequestException("Category data update invalid");
 
-    const category = await this.repository.findOneBy({ id });
-    if (!category) throw new BadRequestException("Category not found");
-
-    const updatedCategory = {
-      ...category,
-      ...updateCategoryDto,
-    };
-
-    const preloadedCategory = await this.repository.preload(updatedCategory);
-    if (!preloadedCategory)
-      throw new ConflictException("Category not preloaded");
-
-    const savedCategory = await this.repository.save(preloadedCategory);
-    if (!savedCategory) throw new ConflictException("Category not updated");
-
-    return savedCategory;
+    return this.repository.updatedCategory(id, updateCategoryDto);
   }
 
   async remove(id: string) {
     if (!id) throw new BadRequestException("Id is required");
-
-    const category = await this.repository.findOneBy({ id });
-    if (!category) throw new BadRequestException("Category not found");
-
-    const categoryRemoved = await this.repository.delete({ id });
-    if (!categoryRemoved) throw new ConflictException("Category not removed");
-
-    return categoryRemoved;
+    return this.repository.deleteCategory(id);
   }
 }
---
+++ src/modules/categories/repositories/categories.repository.ts
@@ -0,0 +1,59 @@
+import { InjectRepository } from "@nestjs/typeorm";
+import { Category } from "../entities/category.entity";
+import { Repository } from "typeorm";
+import { ConflictException, NotFoundException } from "@nestjs/common";
+import { UpdateCategoryDto } from "../dto/update-category.dto";
+import { CreateCategoryDto } from "../dto/create-category.dto";
+
+export class CategoriesRepository {
+  constructor(
+    @InjectRepository(Category)
+    private readonly repository: Repository<Category>,
+  ) {}
+
+  async createCategory(createCategoryDto: CreateCategoryDto) {
+    const categoryExists = await this.repository.findOne({
+      where: { name: createCategoryDto.name },
+    });
+
+    if (categoryExists) {
+      throw new ConflictException("Category already exists");
+    }
+
+    const categoryCreated = this.repository.create(createCategoryDto);
+    return await this.repository.save(categoryCreated);
+  }
+
+  async findAllCategories() {
+    const categories = await this.repository.find();
+    if (!categories || categories.length === 0)
+      throw new NotFoundException("Categories not found");
+  }
+
+  async findCategoryById(id: string) {
+    const category = await this.repository.findOneBy({ id });
+    if (!category) throw new NotFoundException("Category not found");
+
+    return category;
+  }
+
+  async updatedCategory(id: string, updateCategoryDto: UpdateCategoryDto) {
+    const categoryPrealoaded = await this.repository.preload({
+      id,
+      ...updateCategoryDto,
+    });
+
+    if (!categoryPrealoaded) throw new NotFoundException("Category Not Found");
+
+    return await this.repository.save(categoryPrealoaded);
+  }
+
+  async deleteCategory(id: string) {
+    const category = await this.repository.findOneBy({ id });
+    if (!category) throw new NotFoundException("Category not found");
+
+    await this.repository.delete(id);
+
+    return category;
+  }
+}
