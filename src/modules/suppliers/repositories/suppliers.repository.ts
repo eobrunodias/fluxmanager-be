d3b589a907c78902e5f408fc08dbed7eefebe0d8
Bruno Dias <brunodias.contact@gmail.com>
2026-01-15 13:36:09 +0000

    feat: implement SuppliersRepository and SuppliersService with CRUD operations and error handling

---
+++ src/modules/suppliers/repositories/suppliers.repository.ts
@@ -0,0 +1,72 @@
+import { Repository } from "typeorm";
+import { Supplier } from "../entities/supplier.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CreateSupplierDto } from "../dto/create-supplier.dto";
+import { UpdateSupplierDto } from "../dto/update-supplier.dto";
+import { ConflictException, NotFoundException } from "@nestjs/common";
+import { ProductsRepository } from "src/modules/products/repositories/products.repository";
+
+export class SuppliersRepository {
+  constructor(
+    @InjectRepository(Supplier)
+    private readonly supplierRepository: Repository<Supplier>,
+    private readonly productRepository: ProductsRepository,
+  ) {}
+
+  async createSupplier(
+    createSupplierDto: CreateSupplierDto,
+    productId: string,
+  ) {
+    const produto = await this.productRepository.findProductById(productId);
+
+    const supplierExists = await this.supplierRepository.findOneBy({
+      products: {
+        id: productId,
+      },
+    });
+
+    if (supplierExists) throw new ConflictException("Supplier already exists");
+
+    const supplierCreated = this.supplierRepository.create({
+      ...createSupplierDto,
+      products: [produto],
+    });
+
+    return await this.supplierRepository.save(supplierCreated);
+  }
+
+  async findAllSuppliers() {
+    const suppliers = await this.supplierRepository.find();
+    if (!suppliers || suppliers.length === 0)
+      throw new NotFoundException("No suppliers found");
+
+    return suppliers;
+  }
+
+  async findSupplierById(id: string) {
+    const supplier = await this.supplierRepository.findOneBy({ id });
+    if (!supplier) throw new NotFoundException("Supplier not founds");
+
+    return supplier;
+  }
+
+  async updatedSupplier(id: string, updateSupplierDto: UpdateSupplierDto) {
+    const supplierPreloaded = await this.supplierRepository.preload({
+      id,
+      ...updateSupplierDto,
+    });
+
+    if (!supplierPreloaded) throw new NotFoundException("Supplier not found");
+
+    return await this.supplierRepository.save(supplierPreloaded);
+  }
+
+  async deleteSupplier(id: string) {
+    const supplier = await this.supplierRepository.findOneBy({ id });
+    if (!supplier) throw new NotFoundException("Supplier not found");
+
+    await this.supplierRepository.delete(id);
+
+    return supplier;
+  }
+}
--- src/modules/suppliers/suppliers.service.ts
+++ src/modules/suppliers/suppliers.service.ts
@@ -1,88 +1,27 @@
-import {
-  BadRequestException,
-  ConflictException,
-  Injectable,
-  NotFoundException,
-} from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { CreateSupplierDto } from "./dto/create-supplier.dto";
 import { UpdateSupplierDto } from "./dto/update-supplier.dto";
-import { Supplier } from "./entities/supplier.entity";
-import { Repository } from "typeorm";
-import { InjectRepository } from "@nestjs/typeorm";
+import { SuppliersRepository } from "./repositories/suppliers.repository";
 
 @Injectable()
 export class SuppliersService {
-  constructor(
-    @InjectRepository(Supplier)
-    private readonly repository: Repository<Supplier>,
-  ) {}
+  constructor(private readonly repository: SuppliersRepository) {}
 
   async create(createSupplierDto: CreateSupplierDto, productId: string) {
     if (!createSupplierDto)
       throw new BadRequestException("Invalid supplier data");
     if (!productId) throw new BadRequestException("productId is invalid");
 
-    const { address, email, name, phone } = createSupplierDto;
-
-    const existing = await this.repository.findOne({
-      where: [
-        {
-          address,
-        },
-        {
-          email,
-        },
-        {
-          name,
-        },
-        { phone },
-      ],
-    });
-
-    if (existing) {
-      if (address === existing.address)
-        throw new ConflictException("Address already exists");
-      if (email === existing.email)
-        throw new ConflictException("Email already exists");
-      if (name === existing.name)
-        throw new ConflictException("Name already exists");
-      if (phone === existing.phone)
-        throw new ConflictException("Phone already exists");
-    }
-
-    const supplier = {
-      product: {
-        id: productId,
-      },
-      ...createSupplierDto,
-    };
-
-    const supplierCreated = this.repository.create(supplier);
-    if (!supplierCreated) throw new ConflictException("Supplier not created");
-
-    const supplierSaved = await this.repository.save(supplierCreated);
-    if (!supplierSaved) throw new ConflictException("Supplier not saved");
-
-    return supplierSaved;
+    return this.repository.createSupplier(createSupplierDto, productId);
   }
 
   async findAll() {
-    const suppliers = await this.repository.find();
-
-    if (!suppliers || suppliers.length === 0) {
-      throw new NotFoundException("No suppliers found");
-    }
-
-    return suppliers;
+    return this.repository.findAllSuppliers();
   }
 
   async findOne(id: string) {
     if (!id) throw new BadRequestException("Id is required");
-
-    const supplier = await this.repository.findOneBy({ id });
-    if (!supplier) throw new NotFoundException("Supplier not found");
-
-    return supplier;
+    return this.repository.findSupplierById(id);
   }
 
   async update(id: string, updateSupplierDto: UpdateSupplierDto) {
@@ -90,33 +29,11 @@ export class SuppliersService {
     if (!updateSupplierDto)
       throw new BadRequestException("Body data supplier is required");
 
-    const supplier = await this.repository.findOneBy({ id });
-    if (!supplier) throw new NotFoundException("Supplier not found");
-
-    const supplierUpdated = {
-      ...supplier,
-      ...updateSupplierDto,
-    };
-
-    const supplierPreloaded = await this.repository.preload(supplierUpdated);
-    if (!supplierPreloaded)
-      throw new ConflictException("Supplier not preloaded");
-
-    const supplierSaved = await this.repository.save(supplierPreloaded);
-    if (!supplierSaved) throw new ConflictException("Supplier not saved");
-
-    return supplierSaved;
+    return this.repository.updatedSupplier(id, updateSupplierDto);
   }
 
   async remove(id: string) {
     if (!id) throw new BadRequestException("Id is required");
-
-    const supplier = await this.repository.findOneBy({ id });
-    if (!supplier) throw new NotFoundException("Supplier not found");
-
-    const supplierDeleted = await this.repository.delete({ id });
-    if (!supplierDeleted) throw new ConflictException("Supplier not deleted");
-
-    return supplier;
+    return this.repository.findSupplierById(id);
   }
 }
